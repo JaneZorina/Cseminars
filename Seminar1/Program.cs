a3b4067717237bf21e29977b0531eb818fdb1e25
unknown <gurovae@yahoo.com>
2022-12-16 22:14:22 +0300

    seminar1 folder added

---
+++ Seminar1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 1
+
+/*Console.Write("input a first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else 
+{
+    Console.WriteLine("no!");
+}
+*/
+
+//Задача 2
+/*
+Console.Write("input a number: ");
+int number=Convert.ToInt32(Console.ReadLine());
+int current = -number;
+while (current <= number)
+{
+    Console.Write(current + "  ");
+    current = current + 1;
+}
+*/
+
+
