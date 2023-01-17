639c859f882f784f075d18ba2797bd4243536395
unknown <gurovae@yahoo.com>
2023-01-17 22:46:33 +0300

    partly homework5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i< size; i++)
+        array[i] = new Random().Next(minValue,maxValue);
+    return array;    
+}
+
+int FindEven (int[] array)
+{
+    int count=0;
+    for (int i =0 ; i< array.Length; i++)
+    {
+        if (array[i]%2==0) count ++;
+    }
+    return count;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i]+ " ");
+    
+    Console.WriteLine();    
+}
+
+Console.Write("Input a lenght of array: ");
+int lenght= Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a tree digit min possitive value: ");
+int minValue= Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a tree digit max possitive value: ");
+int maxValue= Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(lenght, minValue, maxValue);
+ShowArray(newArray);
+int result = FindEven(newArray);
+Console.Write(result);
