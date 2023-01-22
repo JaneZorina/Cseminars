374bda9da4adb691c535f55905ce17012ec05ca8
unknown <gurovae@yahoo.com>
2023-01-22 22:39:38 +0300

    homework 6 done

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+/*
+int Possitive(int size)
+{
+    int count =0;
+    for(int i=0; i < size; i++) 
+    {
+        Console.Write("Input a number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+            if (b>0 ) 
+            count++;
+     }
+    return count;
+} 
+
+Console.Write("How many numbers would you like to enter: ");
+int size  = Convert.ToInt32(Console.ReadLine());
+int result= Possitive(size);
+Console.Write($"Quantity of possitive numbers is {result}");
+*/
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+Console.Write("Enter k1 : ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1 : ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2 : ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x= (b2-b1)/(k1-k2);
+double y= k1*x +b1;
+Console.WriteLine($"Lines are crossing in point with oordinates {x} ; {y}");
+*/
+
+
+
